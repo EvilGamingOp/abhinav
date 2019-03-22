6abd7aba598b86f7c999039f14fff7d32ce07491
R3DLIZ4RD <41535130+R3DLIZ4RD@users.noreply.github.com>
2019-03-22 21:58:52 +0500

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -17,7 +17,7 @@ from tg_bot.modules import ALL_MODULES
 from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
-PM_START_TEXT = """ NO ONE GONNA HELP YOU
+PM_START_TEXT = """ 
 
 
 """
